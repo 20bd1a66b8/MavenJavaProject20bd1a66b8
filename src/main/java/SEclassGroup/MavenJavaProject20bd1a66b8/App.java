f11d981112867bff939e7e6e8c01bd9341d5c571
Aishwarya <aishwarya.yadugani@gmail.com>
2022-11-16 22:41:02 +0530

    First commit to maven java project 20bd1a66b8

---
+++ src/main/java/SEclassGroup/MavenJavaProject20bd1a66b8/App.java
@@ -0,0 +1,13 @@
+package SEclassGroup.MavenJavaProject20bd1a66b8;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "This is the Maven Java Project in Eclipse" );
+    }
+}
